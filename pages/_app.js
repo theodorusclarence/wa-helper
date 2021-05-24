6b4a99bd881c943714f4e1be36e9bda61b2323ac
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-24 17:20:47 +0700

    feat: add splitbee click count

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  // Splitbee to track link clicks (only counter, not storing any links)
+  if (typeof window !== 'undefined') {
+    window.splitbee?.track('Link Click');
+  }
+
   return (
     <>
       <Component {...pageProps} />
